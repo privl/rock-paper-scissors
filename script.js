d43deec96924824e35816698b70428ab6d2e5d36
someb <someb@test.com>
2024-01-08 23:49:54 +0800

    Add more style and improve the functions

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const result = document.querySelector('.result');
+const computerChoice = document.querySelector('.computer-selection');
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+
+rock.addEventListener('click', () => {playRound('rock', getComputerChoice())});
+paper.addEventListener('click', () => {playRound('paper', getComputerChoice())});
+scissors.addEventListener('click', () => {playRound('scissors', getComputerChoice())});
+
+
+function playRound(playerSelection, computerSelection) {
+    switch (computerSelection) {
+        case 'rock':
+            computerChoice.textContent = '✊';
+            break;
+        case 'paper':
+            computerChoice.textContent = '🖐';
+            break;
+        case 'scissors':
+            computerChoice.textContent = '✌';
+            break;
+    }
+
+}
+
 function getComputerChoice() {
 
     let random = Math.floor(Math.random() * 3);
@@ -16,8 +42,4 @@ function getComputerChoice() {
     }
 
     return computerSelection;
-}
-
-function playRound(playerSelection, computerSelection) {
-    
 }
